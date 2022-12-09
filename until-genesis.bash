0bbda0e4084e242aef90ae5d1048e9bdbe7a31b7
Jacob Gadikian <jacobgadikian@gmail.com>
2022-12-09 18:54:13 +0700

    decompose a bit more

--- until-genesis.bash
+++ until-genesis.bash
@@ -8,6 +8,7 @@ git clone https://github.com/Team-Kujira/core
 # enter the repository
 cd core || exit
 make build
+cd ../
 
 
 # create keys for testing
@@ -23,6 +24,7 @@ make build
 ./core/build/kujirad --home data init test-validator
 
 # inspect genesis and ensure the network will start
+# basically change the denom to ukuji from "stake"
 cp data/config/genesis.json data/config/genesis.json.orig
 nvim data/config/genesis.json
 cp data/config/genesis.json data/config/genesis.json.fixup
